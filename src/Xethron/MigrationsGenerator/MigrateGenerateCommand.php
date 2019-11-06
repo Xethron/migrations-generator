5b75535ed80f57796d554f9296dbd55ca2e21f1e
Zacchaeus Bolaji <bolaji@prepclassng.com>
2019-11-06 17:22:21 +0100

    make ignore-created-migration flag

--- src/Xethron/MigrationsGenerator/MigrateGenerateCommand.php
+++ src/Xethron/MigrationsGenerator/MigrateGenerateCommand.php
@@ -427,7 +427,7 @@ class MigrateGenerateCommand extends GeneratorCommand {
 			['connection', 'c', InputOption::VALUE_OPTIONAL, 'The database connection to use.', $this->config->get( 'database.default' )],
 			['tables', 't', InputOption::VALUE_OPTIONAL, 'A list of Tables you wish to Generate Migrations for separated by a comma: users,posts,comments'],
 			['ignore', 'i', InputOption::VALUE_OPTIONAL, 'A list of Tables you wish to ignore, separated by a comma: users,posts,comments' ],
-            ['ignore-created-migration', 'icm', InputOption::VALUE_OPTIONAL, 'Flag to ignore migrations already created' ],
+            ['ignore-created-migration', 'icm', null, 'Flag to ignore migrations already created' ],
             ['path', 'p', InputOption::VALUE_OPTIONAL, 'Where should the file be created?'],
 			['templatePath', 'tp', InputOption::VALUE_OPTIONAL, 'The location of the template for this generator'],
 			['defaultIndexNames', null, InputOption::VALUE_NONE, 'Don\'t use db index names for migrations'],
